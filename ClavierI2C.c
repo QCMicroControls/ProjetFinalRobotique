d140466ac22c773a5f115bbbec0d49bbc801300d
darkyden <darkyden@gmail.com>
2023-12-06 14:34:33 -0500

    Update ClavierI2C.c

--- ClavierI2C.c
+++ ClavierI2C.c
@@ -19,7 +19,7 @@ D
 #include "Dallas.h"
 #include "I2C.h"
 
-
+#define INT P3_2
 
 // *************************************************************************************************
 unsigned char ucReadKeyI2C(void)
@@ -35,14 +35,25 @@ unsigned char ucReadKeyI2C(void)
 //   Variables utilisées : Aucun.
 //***************************************************************************************************
 {
-	unsigned int uiAddresse = 0x28;
-  unsigned char ucTouche = 0x20;
-	if (INT == 0)
+	const unsigned char ucTabKeymap[16]= {'1','2','3','A',
+					      '4','5','6','B',
+					      '7','8','9','C',
+					      '*','0','#','D'};
+	
+	unsigned char ucKeymapNew = ' ', ucKeymap = ' ';
+	unsigned int uiAddresse = 0x29;
+	
+	if (!INT)
 	{
-		uiAddresse = uiAddresse + ucI2CLire8Bits(1);//ADDRESSE = 0X28+LECTURE I2C AVEC NACK
-		ucTouche = ucLireMemI2C(uiAddresse);        //TOUCHE LUE DANS LA MEMOIRE I2C A L'ADDRESSE POINTEE
-	} 
-	return ucTouche;
+		vI2CStartBit();
+		vI2CEcrire8Bits(uiAddresse);
+		ucKeymapNew = ucI2CLire8Bits(1);		
+		vI2CStopBit();
+		
+		ucKeymap = ucTabKeymap[ucKeymapNew];
+	}	
+	return ucKeymap  ;
+}
 }
 // **************************************************************************************************
 void vTraiteTouche (unsigned char ucTouche)
