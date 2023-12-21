d2a3bb0b4150dfb4e6f49525d1cdebea8e6548e8
C.O.LGIT <96859746+QCMicroControls@users.noreply.github.com>
2023-12-21 15:39:23 -0500

    Update ProjetMain.asm

--- ProjetMain.asm
+++ ProjetMain.asm
@@ -104,9 +104,9 @@
 #define      ADCBALANCE       0xDD     
 
 ;************************************ Buffer ************************************
-#define 	 RXINT  	   	   	PIR1,RCIF ;
-#define    MASKIN            	0x07
-#define	 SUBST			STATUS,Z;
+#define	     RXINT  	   	   	PIR1,RCIF ;
+#define      MASKIN            	0x07
+#define	     SUBST			STATUS,Z;
 
 ;***** VARIABLE DEFINITIONS  **************************************************
 w_temp        EQU     0x71     ; variable used for context saving 
@@ -174,10 +174,8 @@ FSR_temp      EQU     0x74     ; variable used for context saving
      vADCX
      vADCY
      vTrameChecksum
+     vTrameChecksumCheck
     
-  
-
-     
      endc
 
 
@@ -201,10 +199,9 @@ Main
      call    ResetPCA9685
      call    SetFreqPCA9685
 
-     
-    clrf vTramePTRIn
-    clrf vTramePTROut	
-    BCF RXINT
+    clrf     vTramePTRIn
+    clrf     vTramePTROut	
+    BCF	     RXINT
 
 ;Boucle127x256
      movlw   0x7F
@@ -215,29 +212,37 @@ Main
      
 Encore    
     ;boucle 
-
-    decfsz vBoucleHigh, F
-    goto Encore
-
-    decfsz  vBoucleLow, F
-    goto Encore
-     
-    movlw   0x7F
-    movwf   vBoucleHigh 
-    movlw   0xff
-    movwf   vBoucleLow
-     
-    call LireCoord 
-    call LirePince
-    call LireBalance    
-    call TransmetTrame
-
-    call TransmetMoteursPWM
     
-    call Delai1mS
-    ;call TraitementBuffer
+   
+    decfsz   vBoucleHigh, F
+    goto     Encore
+
+    decfsz   vBoucleLow, F
+    goto     Encore
+    
+    movlw    0x7F
+    movwf    vBoucleHigh 
+    movlw    0xff
+    movwf    vBoucleLow
+    
+    call     LireCoord 
+    call     LirePince
+    call     LireBalance    
+    call     TransmetTrame
+    ;call     TraitementBuffer
+    ;call     Delai1mS
+
+    ;call     TrameChecksumReceive
 
     
+;    movfw    vTrameChecksumCheck 
+ ;   subwf    vTrameChecksum, 1
+  ;  btfsc    STATUS,Z
+   ; Goto     BadCheckSum
+   ; call     TransmetMoteursPWM
+    
+
+;BadCheckSum 
     goto    Encore
 
 ;*********************************routines*************************************
@@ -265,22 +270,22 @@ Encore
 ;
 ;******************************************************************************
 TransmetTrame
-    movlw   0x47    ;G 
-    call    Tx232
-    movlw   0x4f   ;O
-    call    Tx232
-    movfw   vADCX
-    call    Tx232
-    movfw   vADCY
-    call    Tx232
-    movfw   vPincePres
-    call    Tx232
-    movfw   vBalancePoid
-    call    Tx232
-    movlw   0x00
-    call    Tx232
-    movfw   vTrameChecksum
-    call    Tx232
+    movlw	 0x47    ;G 
+    call	 Tx232
+    movlw	 0x4f   ;O
+    call	 Tx232
+    movfw	 vADCX
+    call	 Tx232
+    movfw	 vADCY
+    call	 Tx232
+    movfw	 vPincePres
+    call	 Tx232
+    movfw	 vBalancePoid
+    call	 Tx232
+    movlw	 0x00
+    call	 Tx232
+    movfw	 vTrameChecksum
+    call	 Tx232
     return
 ; fin routine Transmettrame----------------------------------------------------
 
@@ -301,19 +306,19 @@ TransmetTrame
 ;
 ;******************************************************************************
 TrameChecksumSend
-    clrf    vTrameChecksum
-    movlw   0x47
-    addwf   vTrameChecksum, F
-    movlw   0x4f
-    addwf   vTrameChecksum, F
-    movfw   vADCX
-    addwf   vTrameChecksum, F
-    movfw   vADCY
-    addwf   vTrameChecksum, F
-    movfw   vPincePres
-    addwf   vTrameChecksum, F
-    movfw   vBalancePoid
-    addwf   vTrameChecksum, F
+    clrf     vTrameChecksum
+    movlw    0x47
+    addwf    vTrameChecksum, F
+    movlw    0x4f
+    addwf    vTrameChecksum, F
+    movfw    vADCX
+    addwf    vTrameChecksum, F
+    movfw    vADCY
+    addwf    vTrameChecksum, F
+    movfw    vPincePres
+    addwf    vTrameChecksum, F
+    movfw    vBalancePoid
+    addwf    vTrameChecksum, F
     return
     
     ;*************************************Tx232************************************
@@ -333,50 +338,51 @@ TrameChecksumSend
 ;
 ;******************************************************************************
 TrameChecksumReceive
-    clrf    vTrameChecksum
-    movlw   0x47
-    addwf   vTrameChecksum, F
-    movlw   0x4f
-    addwf   vTrameChecksum, F
-    movfw   vBase
-    addwf   vTrameChecksum, F
-    movfw   vEpaule
-    addwf   vTrameChecksum, F
-    movfw   vCoude
-    addwf   vTrameChecksum, F
-    movfw   vPoignet
-    addwf   vTrameChecksum, F
-    movfw   vPince
-    addwf   vTrameChecksum, F
-   return
+    clrf     vTrameChecksumCheck
+    movlw    0x47
+    addwf    vTrameChecksumCheck, F
+    movlw    0x4f
+    addwf    vTrameChecksumCheck, F
+    movfw    vBase
+    addwf    vTrameChecksumCheck, F
+    movfw    vEpaule
+    addwf    vTrameChecksumCheck, F
+    movfw    vCoude
+    addwf    vTrameChecksumCheck, F
+    movfw    vPoignet
+    addwf    vTrameChecksumCheck, F
+    movfw    vPince
+    addwf    vTrameChecksumCheck, F
+    return
     
     
 TransmetMoteursPWM
-   movlw 0x00
-   movwf vMoteur
-   movlw 0x68
-   movwf vPwm
-   call  SetPWMPCA9685
-   movlw 0x01
-   movwf vMoteur
-   movlw 0x68
-   movwf vPwm
-   call  SetPWMPCA9685
-   movlw 0x02
-   movwf vMoteur
-   movlw 0x68
-   movwf vPwm
-   call  SetPWMPCA9685
-   movlw 0x03
-   movwf vMoteur
-   movlw 0x68
-   movwf vPwm
-   call  SetPWMPCA9685
-   movlw 0x04
-   movwf vMoteur
-   movlw 0xff
-   movwf vPwm
-   call  SetPWMPCA9685
+    movlw    0x00
+    movwf    vMoteur
+    movfw    vBase
+    movwf    vPwm
+    call     SetPWMPCA9685
+    movlw    0x01
+    movwf    vMoteur
+    movfw    vEpaule
+    movwf    vPwm
+    call     SetPWMPCA9685
+    movlw    0x02
+    movwf    vMoteur
+    movfw    vCoude
+    movwf    vPwm
+    call     SetPWMPCA9685
+    movlw    0x03
+    movwf    vMoteur
+    movfw    vPoignet
+    movwf    vPwm
+    call     SetPWMPCA9685
+    movlw    0x04
+    movwf    vMoteur
+    movfw    vPince
+    movwf    vPwm
+    call     SetPWMPCA9685
+    return
    
 ;*******************************************************************************
 ;                                 routines standard                           : 
@@ -403,20 +409,23 @@ TransmetMoteursPWM
 ;                                               
 ;******************************************************************************
 InitPic
-     bcf     STATUS, RP1       ; Pour s'assurer d'Ãªtre dans les bank 0 et 1 
-     BANK1                     ; Select Bank1        
-     bcf     INTCON,GIE        ; DÃ©sactive les interruptions        
-     clrf    ANSEL             ; DÃ©sactive les convertisseurs reg ANSEL 0x9B        
-     movlw   b'01111000'       ; osc internal 8 Mhz
-     movwf   OSCCON
-     movlw   b'11111111'       ; Remplacer les x par des 1 ou 0.
-     movwf   TRISA             ; PortA en entree         
-     movlw   b'11100100'       ; Bits en entrÃ©es sauf,
-     movwf   TRISB             ; RB3 (Led1), RB4 (Led2) en sortie.
-     movlw   0x00              ; Configure RA0 as analog input
-     movwf   ADCON1      
-     BANK0                     ; Select Bank0
-     return
+    bcf      STATUS, RP1       ; Pour s'assurer d'Ãªtre dans les bank 0 et 1 
+    BANK1                     ; Select Bank1        
+    bsf      INTCON, PEIE 
+    bsf	    PIE1,RCIE
+    clrf     ANSEL             ; DÃ©sactive les convertisseurs reg ANSEL 0x9B        
+    movlw    b'01111000'       ; osc internal 8 Mhz
+    movwf    OSCCON
+    movlw    b'11111111'       ; Remplacer les x par des 1 ou 0.
+    movwf    TRISA             ; PortA en entree         
+    movlw    b'11100100'       ; Bits en entrÃ©es sauf,
+    movwf    TRISB             ; RB3 (Led1), RB4 (Led2) en sortie.
+    movlw    0x00              ; Configure RA0 as analog input
+    movwf    ADCON1      
+    bsf      INTCON,GIE        ;active les interruptions   
+    BANK0                  ; Select Bank0
+    bcf      PIR1,RCIF
+    return
 
 ; fin routine InitPic ---------------------------------------------------------
 
@@ -426,42 +435,51 @@ InitPic
 ;****************************** Interruption **********************************
 Interruption
 
-     btfss	PIR1,RCIF
-     goto	IntOut
-     movwf      w_temp         ; save off current W register contents
-     movfw      STATUS       ; move STATUS register into W register
-     movwf	status_temp    ; save off contents of STATUS register
-     movfw      PCLATH       ; move PCLATH register into W register
-     movwf	pclath_temp    ; save off contents of PCLATH register
+
+    movwf    w_temp         ; save off current W register contents
+    movfw    STATUS       ; move STATUS register into W register
+    movwf    status_temp    ; save off contents of STATUS register
+    movfw    PCLATH       ; move PCLATH register into W register
+    movwf    pclath_temp    ; save off contents of PCLATH register
+
 
 ; isr code can go here or be located as a call subroutine elsewhere
 
-	btfss RXINT
-	goto  Interruption
-	call Rx232
-	BCF RXINT
-    movfw vTramePTRIn
-    andlw MASKIN
-    movwf vTramePTRIn
-    addlw 0x20
-    movwf FSR
-    movfw vReceive
-    movwf INDF	
-    INCF vTramePTRIn
-    movfw vTramePTRIn
-    andlw MASKIN
-    movwf vTramePTRIn
+    btfss    PIR1,RCIF
+    goto     IntOut    
+    
+    btfss    RXINT
+    goto     Interruption
+    call     Rx232
+    BCF	     RXINT
+    movfw    vTramePTRIn
+    andlw    MASKIN
+    movwf    vTramePTRIn
+    addlw    0x20
+    movwf    FSR
+    movfw    vReceive
+    movwf    INDF	
+    INCF     vTramePTRIn
+    movfw    vTramePTRIn
+    andlw    MASKIN
+    movwf    vTramePTRIn
 
-     movfw     FSR_temp
-     movwf     FSR
-     movf      pclath_temp,w  ; retrieve copy of PCLATH register
-     movwf     PCLATH         ; restore pre-isr PCLATH register contents
-     movf      status_temp,w  ; retrieve copy of STATUS register
-     movwf     STATUS         ; restore pre-isr STATUS register contents
-     swapf     w_temp,f
-     swapf     w_temp,w       ; restore pre-isr W register contents
+    movfw    FSR_temp
+    movwf    FSR
+    
+    
+IntOut    
+    movf     pclath_temp,w  ; retrieve copy of PCLATH register
+    movwf    PCLATH         ; restore pre-isr PCLATH register contents
+    movf     status_temp,w  ; retrieve copy of STATUS register
+    movwf    STATUS         ; restore pre-isr STATUS register contents
+    swapf    w_temp,f
+    swapf    w_temp,w       ; restore pre-isr W register contents
 
-IntOut
+    
+
+    
+
      retfie                   ; return from interrupt
 
 
