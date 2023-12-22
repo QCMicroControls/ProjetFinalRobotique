ea5e9d7600aa5ceeb23a0aae3e4c24fbcb485b6b
C.O.LGIT <96859746+QCMicroControls@users.noreply.github.com>
2023-12-22 13:50:03 -0500

    Update RoutineRxInt.asm

--- RoutineRxInt.asm
+++ RoutineRxInt.asm
@@ -1,108 +1,110 @@
 
+
 TraitementBuffer 
 
-	movfw vTramePTRIn
-	subwf vTramePTROut,0
-	btfss SUBST
-	goto FinReceive
-
-Case0 
+	movfw	vTramePTRIn
+	subwf	vTramePTROut,W
+	btfsc	STATUS, Z
+	goto	FinReceive
+	
+Case0	
 	movlw 0x00
 	subwf vIndiceTrame,W
-	btfss SUBST
+	btfss STATUS, Z
 	goto Case1
 	goto LireCar1
 
 Case1
 	movlw 0x01
 	subwf vIndiceTrame,W
-	btfss SUBST
+	btfss STATUS, Z
 	goto Case2
 	goto LireCar2
 
 Case2
 	movlw 0x02
 	subwf vIndiceTrame,W
-	btfss SUBST
+	btfss STATUS, Z
 	goto Case3
 	goto LireCar3
 
 Case3
 	movlw 0x03
 	subwf vIndiceTrame,W
-	btfss SUBST
+	btfss STATUS, Z
 	goto Case4
 	goto LireCar4
 	
 Case4
 	movlw 0x04
 	subwf vIndiceTrame,W
-	btfss SUBST
+	btfss STATUS, Z
 	goto Case5
 	goto LireCar5
 	
 Case5
 	movlw 0x05
 	subwf vIndiceTrame,W
-	btfss SUBST
+	btfss STATUS, Z
 	goto Case6
 	goto LireCar6
 	
 Case6
+	
 	movlw 0x06
 	subwf vIndiceTrame,W
-	btfss SUBST
+	btfss STATUS, Z
 	goto Case7
 	goto LireCar7
 
 Case7
 	movlw 0x07
 	subwf vIndiceTrame,W
-	btfss SUBST
+	btfss STATUS, Z
 	goto Case1
 	goto LireCar8
 
 	
-LireCar1	
+LireCar1
 	movfw vTramePTROut
 	andlw 0x07
 	addlw 0x20
 	movwf FSR
 	movfw INDF
 	movwf vReceive
-	incf  vTramePTROut
+	incf  vTramePTROut,F
 	movlw 0x07
-	andwf vTramePTROut
+	andwf vTramePTROut,F
 	movlw 0x20
 	movwf INDF
 	movlw 0x47
-	subwf vReceive
-	btfss SUBST
+	subwf vReceive, W
+	btfss STATUS, Z
 	goto  FinReceive
-	incf  vIndiceTrame
+	incf  vIndiceTrame, F
 	movlw 0x07
-	andwf vIndiceTrame
+	andwf vIndiceTrame, F
 	goto  FinReceive
 	
-LireCar2	
+LireCar2
 	movfw vTramePTROut
 	andlw 0x07
 	addlw 0x20
 	movwf FSR
 	movfw INDF
 	movwf vReceive
-	incf  vTramePTROut
+	incf  vTramePTROut,F
 	movlw 0x07
-	andwf vTramePTROut
+	andwf vTramePTROut,F
 	movlw 0x20
 	movwf INDF
 	movlw 0x4F
-	subwf vReceive
-	btfss SUBST
+	subwf vReceive, W
+	btfss STATUS, Z
 	goto  FinReceive
-	incf  vIndiceTrame
+	incf  vIndiceTrame, F
 	movlw 0x07
-	andwf vIndiceTrame
+	andwf vIndiceTrame, F
 	goto  FinReceive
 	
 LireCar3	
@@ -111,73 +113,75 @@ LireCar3
 	addlw 0x20
 	movwf FSR
 	movfw INDF
-	movwf vBase
-	incf  vTramePTROut
+	movwf vBase 
+	incf  vTramePTROut, F
 	movlw 0x07
-	andwf vTramePTROut
-	incf  vIndiceTrame
+	andwf vTramePTROut, F
+	incf  vIndiceTrame, F
 	movlw 0x07
-	andwf vIndiceTrame,f
+	andwf vIndiceTrame, F
 	goto  FinReceive
 
-LireCar4	
+LireCar4
 	movfw vTramePTROut
 	andlw 0x07
 	addlw 0x20
 	movwf FSR
 	movfw INDF
-	movwf vEpaule
-	incf  vTramePTROut
+	movwf vEpaule 
+	incf  vTramePTROut, F
 	movlw 0x07
-	andwf vTramePTROut
-	incf  vIndiceTrame
+	andwf vTramePTROut, F
+	incf  vIndiceTrame, F 
 	movlw 0x07
 	andwf vIndiceTrame,f
 	goto  FinReceive
 	
-LireCar5	
+LireCar5
 	movfw vTramePTROut
 	andlw 0x07
 	addlw 0x20
 	movwf FSR
 	movfw INDF
 	movwf vCoude
-	incf  vTramePTROut
+	incf  vTramePTROut,F
 	movlw 0x07
-	andwf vTramePTROut
-	incf  vIndiceTrame
+	andwf vTramePTROut, F
+	incf  vIndiceTrame, F
 	movlw 0x07
-	andwf vIndiceTrame,f
+	andwf vIndiceTrame, F
 	goto  FinReceive
 	
-LireCar6	
+LireCar6
+	
 	movfw vTramePTROut
 	andlw 0x07
 	addlw 0x20
 	movwf FSR
 	movfw INDF
 	movwf vPoignet
-	incf  vTramePTROut
+	incf  vTramePTROut, F
 	movlw 0x07
-	andwf vTramePTROut
-	incf  vIndiceTrame
+	andwf vTramePTROut, F
+	incf  vIndiceTrame, F
 	movlw 0x07
-	andwf vIndiceTrame,f
+	andwf vIndiceTrame, F
 	goto  FinReceive
 	
-LireCar7	
+LireCar7
+
 	movfw vTramePTROut
 	andlw 0x07
 	addlw 0x20
 	movwf FSR
 	movfw INDF
 	movwf vPince
-	incf  vTramePTROut
+	incf  vTramePTROut, F
 	movlw 0x07
-	andwf vTramePTROut
-	incf  vIndiceTrame
+	andwf vTramePTROut, F
+	incf  vIndiceTrame, F
 	movlw 0x07
-	andwf vIndiceTrame,f
+	andwf vIndiceTrame, F
 	goto  FinReceive
 	
 LireCar8	
@@ -186,14 +190,15 @@ LireCar8
 	addlw 0x20
 	movwf FSR
 	movfw INDF
-	movwf vMoteur6
-	incf  vTramePTROut
+	movwf vTrameChecksum
+	incf  vTramePTROut, F
 	movlw 0x07
-	andwf vTramePTROut
-	incf  vIndiceTrame
+	andwf vTramePTROut, F
+	incf  vIndiceTrame, F
 	movlw 0x07
-	andwf vIndiceTrame,f
+	andwf vIndiceTrame, F
 	goto FinReceive 
+	  
 
 FinReceive
 	return
